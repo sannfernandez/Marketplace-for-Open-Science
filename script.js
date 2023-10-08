139b3d32f4568a9ef1e15e8cb508cf929938c81f
Santiago <santifernandezok1@gmail.com>
2023-10-08 15:28:36 -0300

    por las dudas

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ document.addEventListener("DOMContentLoaded", function () {
         menu.classList.toggle("active");
     });
 });
+
+function filterResources(option) {
+    // Ocultar todos los recursos
+    document.getElementById('papers').style.display = 'none';
+    document.getElementById('revistas').style.display = 'none';
+    document.getElementById('libros').style.display = 'none';
+
+    // Mostrar recursos según la opción seleccionada
+    document.getElementById(option).style.display = 'block';
+}
